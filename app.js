b645ee7165b22046333c42821b43ee8255b46193
Mark Tse <mark.tse@neverendingqs.com>
2016-03-20 18:03:19 -0400

    Added another form to compare.

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 var app = angular.module('neverendingqsPricePerUnitApp', []);
 
 app.controller('PricePerUnitController', ['$scope', function($scope) {
-  $scope.input = {
+  $scope.left = {
     cost: 2.99,
-    unit: 100
+    unit: 10
   }
 
-  $scope.input.change = function(cost, unit) {
-    $scope.input.costperunit = (cost / unit).toFixed(2);
+  $scope.left.change = function(cost, unit) {
+    $scope.left.costperunit = (cost / unit).toFixed(2);
   }
 
-  $scope.input.change($scope.input.cost, $scope.input.unit);
+  $scope.right = {
+    cost: 5.99,
+    unit: 50
+  }
+
+  $scope.right.change = function(cost, unit) {
+    $scope.right.costperunit = (cost / unit).toFixed(2);
+  }
+
+  $scope.left.change($scope.left.cost, $scope.left.unit);
+  $scope.right.change($scope.right.cost, $scope.right.unit);
 }]);
